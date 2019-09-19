c6a0084e9d13197a6f5c99fae2a2756bfcc3c9ff
ChrisGC <christopher.gilbert@sansano.usm.cl>
2019-09-18 22:14:33 -0300

    agregue el thread para el heartbeat
    intente pasarlo a containers de ubuntu y fallé, seguire con el container de python

--- Actividad 2/headnode.py
+++ Actividad 2/headnode.py
@@ -4,40 +4,48 @@ import time
 import threading
 import _thread
 
-message = 'very important data'
-multicast_group = ('224.10.10.10', 10000)
 
-# Create the datagram socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+def thread_nodes():
+    message = 'very important data'
+    multicast_group = ('224.10.10.10', 10000)
 
-# Set a timeout so the socket does not block
-# indefinitely when trying to receive data.
-sock.settimeout(5)
+    # Create the datagram socket
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-# Set the time-to-live for messages to 1 so they do not
-# go past the local network segment.
-ttl = struct.pack('b', 1)
-sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, ttl)
+    # Set a timeout so the socket does not block
+    # indefinitely when trying to receive data.
+    sock.settimeout(5)
 
-try:
-    while True:
-        # Send data to the multicast group
-        print('sending {!r}'.format(message))
-        sent = sock.sendto(message.encode('utf-8'), multicast_group)
+    # Set the time-to-live for messages to 1 so they do not
+    # go past the local network segment.
+    ttl = struct.pack('b', 1)
+    sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, ttl)
 
-        # Look for responses from all recipients
-        print('waiting to receive')
-        while(True):
-            try:
-                data, server = sock.recvfrom(4096)
-                alias = socket.gethostbyaddr(server[0]) #obtengo el nombre del host, ex: node1.network
-                hostip = socket.gethostbyname(alias[0]) #pruebo que efectivamente nodeX.network me de su ip
-                print(alias) #uwu
-                print(hostip) #unu
-                print('received {!r} from {}'.format(data.decode('utf-8'), server))
-            except:
-                print("No more nodes sent ACK")
-                break
-finally:
-    print('closing socket')
-    sock.close()
+    try:
+        while True:
+            # Send data to the multicast group
+            print('sending {!r}'.format(message))
+            sent = sock.sendto(message.encode('utf-8'), multicast_group)
+
+            # Look for responses from all recipients
+            print('waiting to receive')
+            while(True):
+                try:
+                    data, server = sock.recvfrom(4096)
+                    alias = socket.gethostbyaddr(server[0]) #obtengo el nombre del host, ex: node1.network
+                    hostip = socket.gethostbyname(alias[0]) #pruebo que efectivamente nodeX.network me de su ip
+                    print(alias) #uwu
+                    print(hostip) #unu
+                    print('received {!r} from {}'.format(data.decode('utf-8'), server))
+                except:
+                    print("No more nodes sent ACK")
+                    break
+    finally:
+        print('closing socket')
+        sock.close()
+
+def Main():
+    _thread.start_new_thread(thread_nodes)
+
+if __name__ == '__main__':
+    Main()
